8e4e79c4a5babaaba037e35a1891cc3c657d219f
camilamila <84359892+CamilaOrtolane@users.noreply.github.com>
2023-06-09 12:34:43 -0300

    att

--- js/gravar.js
+++ js/gravar.js
@@ -1,61 +1,8 @@
-function validarCPF(cpf) {
-    if (cpf.length !== 11) {
-      return false;
-    }
-    if (!/^\d+$/.test(cpf)) {
-      return false;
-    }
-    if (new Set(cpf.split("")).size === 1) {
-      return false;
-    }
-    return true;
-}
-  function primeiroDigito(cpf) {
-    let peso = 10;
-    let soma = 0;
-  
-    for (let i = 0; i < 9; i++) {
-      soma += parseInt(cpf.charAt(i)) * peso;
-      peso--;
-}
-  
-    const resto = soma % 11;
-    const dgv = resto < 2 ? 0 : 11 - resto;
-  
-    return dgv === parseInt(cpf.charAt(9));
-}
-  
-  function segundoDigito(cpf) {
-    let peso = 11;
-    let soma = 0;
-  
-    for (let i = 0; i < 10; i++) {
-      soma += parseInt(cpf.charAt(i)) * peso;
-      peso--;
-}
-  
-    const resto = soma % 11;
-    const dgv = resto < 2 ? 0 : 11 - resto;
-  
-    return dgv === parseInt(cpf.charAt(10));
-}
-  function salvar() {
-    var cpf = document.getElementById("cpf").value;
-    var verificado = document.getElementById("verificado");
-
-    if (validarCPF(cpf)) {
-      verificado.innerHTML = "CPF válido";
-    } else {
-      verificado.innerHTML = "CPF inválido";
-    }
-}
-
-
 const form = document.getElementById("meuForm");
 const cadastrados = document.getElementById("lista-cadastro");
 const pessoaArray = JSON.parse(localStorage.getItem("pessoaArray")) || [];
 
-alunosArray.forEach((elemento) => {
+pessoaArray.forEach((elemento) => {
   newEvent(elemento);
 });
 
@@ -64,7 +11,7 @@ form.addEventListener("submit", function (e) {
   const pessoa = e.target.elements["pessoa"];
   const email = e.target.elements["email"];
   const celular = e.target.elements["celular"];
-  cpf = e.target.elements["cpf"];
+  const cpf = e.target.elements["cpf"];
 
   const pessoaStorage = {
     pessoa: pessoa.value,
@@ -99,5 +46,3 @@ function newEvent(pessoaADD) {
   newItem.appendChild(numeroItem);
   convidados.appendChild(newItem);
 }
-
-  
